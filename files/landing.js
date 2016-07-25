1605b52bc8c37b4aba915defb3458b60e8334edb
Matthias Geier <mayutamano@gmail.com>
2016-07-25 21:00:55 +0200

    First landing page steps

--- files/landing.js
+++ files/landing.js
@@ -1,7 +1,52 @@
 var Landing = React.createClass({displayName: "Landing",
+  getInitialState: function() {
+    return {};
+  },
+  componentDidMount: function() {
+  },
   render: function() {
-    return React.createElement("div", {className: "col-xs-12 col-md-12", style: {backgroundColor: "#e5e5e5", minHeight: "50%", borderRadius: "5px"}}, 
-      "mooo"
+    return React.createElement("div", {className: "col-xs-12 col-md-12", 
+      style: {backgroundColor: "#e5e5e5", minHeight: "50%",
+      borderRadius: "5px"}}, 
+
+      React.createElement("div", {className: "row"}, 
+        React.createElement("div", {className: "col-xs-4 col-md-4"}, 
+          React.createElement("h4", null, "The what!"), 
+          React.createElement("p", null, 
+            "A platform to roleplay, write, create, dream and share your stories," + ' ' +
+            "adventures and fantasies. Collaborate with friends or likeminded" + ' ' +
+            "people. Create your own universe!"
+          )
+        ), 
+        React.createElement("div", {className: "col-xs-4 col-md-4"}, 
+          React.createElement("h4", null, "The how!"), 
+          React.createElement("p", null, 
+            "Get started by:", 
+            React.createElement("ul", null, 
+              React.createElement("li", null, "Hit the huge ", React.createElement("span", {className: "glyphicon glyphicon-plus", 
+                "aria-hidden": "true"}), " to create your own universe"), 
+              React.createElement("li", null, "Select Stories"), 
+              React.createElement("li", null, "Hit the ", React.createElement("span", {className: "glyphicon glyphicon-plus", 
+                "aria-hidden": "true"}), " for adding a story"), 
+              React.createElement("li", null, "Enter a title and then create it"), 
+              React.createElement("li", null, "Start writing a story diary-style")
+            )
+          )
+        ), 
+        React.createElement("div", {className: "col-xs-4 col-md-4"}, 
+          React.createElement("h4", null, "The collaboration!"), 
+          React.createElement("p", null, 
+            "And here's how to invite friends:", 
+            React.createElement("ul", null, 
+              React.createElement("li", null, "Hit the huge ", React.createElement("span", {className: "glyphicon glyphicon-lock", 
+                "aria-hidden": "true"}), " on your universe"), 
+              React.createElement("li", null, "Hit the ", React.createElement("span", {className: "glyphicon glyphicon-plus", 
+                "aria-hidden": "true"}), " to generate a new Access Key"), 
+              React.createElement("li", null, "Send your friend the URL behind the new key")
+            )
+          )
+        )
+      )
     );
   }
 });
--- jsx/landing.jsx
+++ jsx/landing.jsx
@@ -2,31 +2,51 @@ var Landing = React.createClass({
   getInitialState: function() {
     return {};
   },
-  initSlider(): function() {
-  },
-  sliderStop: function(e) {
-  },
   componentDidMount: function() {
-    this.initSlider();
-  },
-  renderSlider: function() {
-    var elements = [
-      "cookies",
-      "monsters",
-      "sprites"
-    ];
-    return <div className="row" ref="slider" onMouseOver={this.sliderStop}>
-      {elements.map(function(elem) {
-        return <div className="col-xs-12 col-md-12 hidden">{elem}</div>;
-      })}
-    </div>;
   },
   render: function() {
     return <div className="col-xs-12 col-md-12"
       style={{backgroundColor: "#e5e5e5", minHeight: "50%",
       borderRadius: "5px"}}>
 
-      {this.renderSlider()}
+      <div className="row">
+        <div className="col-xs-4 col-md-4">
+          <h4>The what!</h4>
+          <p>
+            A platform to roleplay, write, create, dream and share your stories,
+            adventures and fantasies. Collaborate with friends or likeminded
+            people. Create your own universe!
+          </p>
+        </div>
+        <div className="col-xs-4 col-md-4">
+          <h4>The how!</h4>
+          <p>
+            Get started by:
+            <ul>
+              <li>Hit the huge <span className="glyphicon glyphicon-plus"
+                aria-hidden="true" /> to create your own universe</li>
+              <li>Select Stories</li>
+              <li>Hit the <span className="glyphicon glyphicon-plus"
+                aria-hidden="true" /> for adding a story</li>
+              <li>Enter a title and then create it</li>
+              <li>Start writing a story diary-style</li>
+            </ul>
+          </p>
+        </div>
+        <div className="col-xs-4 col-md-4">
+          <h4>The collaboration!</h4>
+          <p>
+            And here's how to invite friends:
+            <ul>
+              <li>Hit the huge <span className="glyphicon glyphicon-lock"
+                aria-hidden="true" /> on your universe</li>
+              <li>Hit the <span className="glyphicon glyphicon-plus"
+                aria-hidden="true" /> to generate a new Access Key</li>
+              <li>Send your friend the URL behind the new key</li>
+            </ul>
+          </p>
+        </div>
+      </div>
     </div>;
   }
 });
