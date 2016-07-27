f991047de2b1bc53b06bd91f990868ae4cc56536
Matthias Geier <mayutamano@gmail.com>
2016-07-27 22:05:42 +0200

    More landing page changes

--- files/landing.js
+++ files/landing.js
@@ -20,54 +20,102 @@ var Landing = React.createClass({displayName: "Landing",
       style: {backgroundColor: "#e5e5e5", minHeight: "50%",
       borderRadius: "5px", paddingTop: "1em"}}, 
 
-      React.createElement("div", {className: "col-xs-4 col-md-4", style: {border: "1px solid #ddd",
-        backgroundColor: "rgba(255, 255, 255, 0.5)",
-        borderRadius: "5px"}}, 
-        React.createElement("h4", null, "The what!"), 
+      React.createElement("div", {className: "jumbotron"}, 
+        React.createElement("h2", null, "The what!"), 
         React.createElement("p", null, 
           "A platform to roleplay, write, create, dream and share your stories," + ' ' +
           "adventures and fantasies. Collaborate with friends or likeminded" + ' ' +
           "people. Create your own universe!"
         )
       ), 
-      React.createElement("div", {className: "col-xs-4 col-md-4", style: {border: "1px solid #ddd",
-        backgroundColor: "rgba(255, 255, 255, 0.5)",
-        borderRadius: "5px"}}, 
-        React.createElement("h4", null, "The how!"), 
-        React.createElement("p", null, 
-          "Get started by:"
+      React.createElement("div", {className: "row"}, 
+        React.createElement("div", {className: "col-xs-6 col-md-6"}, 
+          React.createElement("div", {className: "jumbotron"}, 
+            React.createElement("h2", null, "The how!"), 
+            React.createElement("p", null, 
+              "Get started by:"
+            ), 
+            React.createElement("ul", null, 
+              React.createElement("li", null, "Hit the huge ", React.createElement("span", {className: "glyphicon glyphicon-plus", 
+                "aria-hidden": "true"}), " to create your own universe"), 
+              React.createElement("li", null, "Select Stories"), 
+              React.createElement("li", null, "Hit the ", React.createElement("span", {className: "glyphicon glyphicon-plus", 
+                "aria-hidden": "true"}), " for adding a story"), 
+              React.createElement("li", null, "Enter a title and then create it"), 
+              React.createElement("li", null, "Start writing a story diary-style")
+            )
+          )
         ), 
-        React.createElement("ul", null, 
-          React.createElement("li", null, "Hit the huge ", React.createElement("span", {className: "glyphicon glyphicon-plus", 
-            "aria-hidden": "true"}), " to create your own universe"), 
-          React.createElement("li", null, "Select Stories"), 
-          React.createElement("li", null, "Hit the ", React.createElement("span", {className: "glyphicon glyphicon-plus", 
-            "aria-hidden": "true"}), " for adding a story"), 
-          React.createElement("li", null, "Enter a title and then create it"), 
-          React.createElement("li", null, "Start writing a story diary-style")
+        React.createElement("div", {className: "col-xs-6 col-md-6"}, 
+          React.createElement("div", {className: "jumbotron"}, 
+            React.createElement("h2", null, "The collaboration!"), 
+            React.createElement("p", null, 
+              "And here's how to invite friends:"
+            ), 
+            React.createElement("ul", null, 
+              React.createElement("li", null, "Hit the huge ", React.createElement("span", {className: "glyphicon glyphicon-lock", 
+                "aria-hidden": "true"}), " on your universe"), 
+              React.createElement("li", null, "Hit the ", React.createElement("span", {className: "glyphicon glyphicon-plus", 
+                "aria-hidden": "true"}), " to generate a new Access Key"), 
+              React.createElement("li", null, "Send your friend the URL behind the new key")
+            )
+          )
         )
       ), 
-      React.createElement("div", {className: "col-xs-4 col-md-4", style: {border: "1px solid #ddd",
-        backgroundColor: "rgba(255, 255, 255, 0.5)",
-        borderRadius: "5px"}}, 
-        React.createElement("h4", null, "The collaboration!"), 
-        React.createElement("p", null, 
-          "And here's how to invite friends:"
-        ), 
-        React.createElement("ul", null, 
-          React.createElement("li", null, "Hit the huge ", React.createElement("span", {className: "glyphicon glyphicon-lock", 
-            "aria-hidden": "true"}), " on your universe"), 
-          React.createElement("li", null, "Hit the ", React.createElement("span", {className: "glyphicon glyphicon-plus", 
-            "aria-hidden": "true"}), " to generate a new Access Key"), 
-          React.createElement("li", null, "Send your friend the URL behind the new key")
-        )
-      ), 
-      React.createElement("div", {className: "col-xs-12 col-md-12 panel-group", id: "accordion"}, 
-        React.createElement("h3", null, "FAQ"), 
-        React.createElement("h4", {onClick: this.collapse}, 
-          React.createElement("a", {href: "#collapse"}, "What is a prop?")
-        ), 
-        React.createElement("div", {className: "well", style: {display: "none"}}, "Yays")
+      React.createElement("div", {className: "row"}, React.createElement("div", {className: "col-xs-12 col-md-12"}, 
+        React.createElement("div", {className: "panel panel-default"}, 
+          React.createElement("div", {className: "panel-heading"}, 
+            React.createElement("h3", null, "FAQ")
+          ), 
+          React.createElement("div", {className: "panel-heading", onClick: this.collapse}, 
+            React.createElement("h4", null, React.createElement("a", {href: "#collapse"}, "What is a prop?"))
+          ), 
+          React.createElement("div", {className: "panel-body", style: {display: "none"}}, 
+            React.createElement("p", null, "It is an way to represent various kinds of elements in your" + ' ' +
+            "universe. By just needing a name, it can be a character," + ' ' +
+            "an animal, an object, anything really that needs properties."), 
+
+            React.createElement("p", null, "Imagine your main character John J. Doe who is 29 years old" + ' ' +
+            "and has brown hair. This is a way to bring him into the" + ' ' +
+            "universe.")
+          ), 
+
+          React.createElement("div", {className: "panel-heading", onClick: this.collapse}, 
+            React.createElement("h4", null, React.createElement("a", {href: "#collapse"}, "How can a prop be used?"))
+          ), 
+          React.createElement("div", {className: "panel-body", style: {display: "none"}}, 
+            React.createElement("p", null, "It only makes sense to define a prop, when it can be used inside" + ' ' +
+            "a story. At the moment you link the prop name in the story" + ' ' +
+            "and hide the link behind a description."), 
+
+            React.createElement("p", null, "Take your main character John J. Doe, who has the prop name", 
+            React.createElement("i", null, "john_j_doe"), ". Reference him in any story by adding" + ' ' +
+            "[john_j_doe|John Doe] and he will appear as a link."), 
+
+            React.createElement("p", null, "The second option, the name for the link is a choice made" + ' ' +
+            "for universes that evolve. Maybe in your progress, John will" + ' ' +
+            "receive a crown, or knighthood. Suddenly in the next story" + ' ' +
+            "he is King John, but changing the link name everywhere would" + ' ' +
+            "be spoiling it.")
+          ), 
+
+          React.createElement("div", {className: "panel-heading", onClick: this.collapse}, 
+            React.createElement("h4", null, React.createElement("a", {href: "#collapse"}, "What is an access key?"))
+          ), 
+          React.createElement("div", {className: "panel-body", style: {display: "none"}}, 
+            React.createElement("p", null, "It is the username equivalent. If you play with a bunch of" + ' ' +
+            "people, try to create as many access keys as you have players.")
+          ), 
+
+          React.createElement("div", {className: "panel-heading", onClick: this.collapse}, 
+            React.createElement("h4", null, React.createElement("a", {href: "#collapse"}, "Are there different access rights?"))
+          ), 
+          React.createElement("div", {className: "panel-body", style: {display: "none"}}, 
+            React.createElement("p", null, "Right, the admin and no admin setup. It is planned to add" + ' ' +
+            "a role each access key, but right now everyone is the" + ' ' +
+            "superadmin. Make backups often if you do not trust your players!")
+          )
+        ))
       )
     );
   }
--- jsx/landing.jsx
+++ jsx/landing.jsx
@@ -20,54 +20,102 @@ var Landing = React.createClass({
       style={{backgroundColor: "#e5e5e5", minHeight: "50%",
       borderRadius: "5px", paddingTop: "1em"}}>
 
-      <div className="col-xs-4 col-md-4" style={{border: "1px solid #ddd",
-        backgroundColor: "rgba(255, 255, 255, 0.5)",
-        borderRadius: "5px"}}>
-        <h4>The what!</h4>
+      <div className="jumbotron">
+        <h2>The what!</h2>
         <p>
           A platform to roleplay, write, create, dream and share your stories,
           adventures and fantasies. Collaborate with friends or likeminded
           people. Create your own universe!
         </p>
       </div>
-      <div className="col-xs-4 col-md-4" style={{border: "1px solid #ddd",
-        backgroundColor: "rgba(255, 255, 255, 0.5)",
-        borderRadius: "5px"}}>
-        <h4>The how!</h4>
-        <p>
-          Get started by:
-        </p>
-        <ul>
-          <li>Hit the huge <span className="glyphicon glyphicon-plus"
-            aria-hidden="true" /> to create your own universe</li>
-          <li>Select Stories</li>
-          <li>Hit the <span className="glyphicon glyphicon-plus"
-            aria-hidden="true" /> for adding a story</li>
-          <li>Enter a title and then create it</li>
-          <li>Start writing a story diary-style</li>
-        </ul>
+      <div className="row">
+        <div className="col-xs-6 col-md-6">
+          <div className="jumbotron">
+            <h2>The how!</h2>
+            <p>
+              Get started by:
+            </p>
+            <ul>
+              <li>Hit the huge <span className="glyphicon glyphicon-plus"
+                aria-hidden="true" /> to create your own universe</li>
+              <li>Select Stories</li>
+              <li>Hit the <span className="glyphicon glyphicon-plus"
+                aria-hidden="true" /> for adding a story</li>
+              <li>Enter a title and then create it</li>
+              <li>Start writing a story diary-style</li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-xs-6 col-md-6">
+          <div className="jumbotron">
+            <h2>The collaboration!</h2>
+            <p>
+              And here's how to invite friends:
+            </p>
+            <ul>
+              <li>Hit the huge <span className="glyphicon glyphicon-lock"
+                aria-hidden="true" /> on your universe</li>
+              <li>Hit the <span className="glyphicon glyphicon-plus"
+                aria-hidden="true" /> to generate a new Access Key</li>
+              <li>Send your friend the URL behind the new key</li>
+            </ul>
+          </div>
+        </div>
       </div>
-      <div className="col-xs-4 col-md-4" style={{border: "1px solid #ddd",
-        backgroundColor: "rgba(255, 255, 255, 0.5)",
-        borderRadius: "5px"}}>
-        <h4>The collaboration!</h4>
-        <p>
-          And here's how to invite friends:
-        </p>
-        <ul>
-          <li>Hit the huge <span className="glyphicon glyphicon-lock"
-            aria-hidden="true" /> on your universe</li>
-          <li>Hit the <span className="glyphicon glyphicon-plus"
-            aria-hidden="true" /> to generate a new Access Key</li>
-          <li>Send your friend the URL behind the new key</li>
-        </ul>
-      </div>
-      <div className="col-xs-12 col-md-12 panel-group" id="accordion">
-        <h3>FAQ</h3>
-        <h4 onClick={this.collapse}>
-          <a href="#collapse">What is a prop?</a>
-        </h4>
-        <div className="well" style={{display: "none"}}>Yays</div>
+      <div className="row"><div className="col-xs-12 col-md-12">
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3>FAQ</h3>
+          </div>
+          <div className="panel-heading" onClick={this.collapse}>
+            <h4><a href="#collapse">What is a prop?</a></h4>
+          </div>
+          <div className="panel-body" style={{display: "none"}}>
+            <p>It is an way to represent various kinds of elements in your
+            universe. By just needing a name, it can be a character,
+            an animal, an object, anything really that needs properties.</p>
+
+            <p>Imagine your main character John J. Doe who is 29 years old
+            and has brown hair. This is a way to bring him into the
+            universe.</p>
+          </div>
+
+          <div className="panel-heading" onClick={this.collapse}>
+            <h4><a href="#collapse">How can a prop be used?</a></h4>
+          </div>
+          <div className="panel-body" style={{display: "none"}}>
+            <p>It only makes sense to define a prop, when it can be used inside
+            a story. At the moment you link the prop name in the story
+            and hide the link behind a description.</p>
+
+            <p>Take your main character John J. Doe, who has the prop name
+            <i>john_j_doe</i>. Reference him in any story by adding
+            [john_j_doe|John Doe] and he will appear as a link.</p>
+
+            <p>The second option, the name for the link is a choice made
+            for universes that evolve. Maybe in your progress, John will
+            receive a crown, or knighthood. Suddenly in the next story
+            he is King John, but changing the link name everywhere would
+            be spoiling it.</p>
+          </div>
+
+          <div className="panel-heading" onClick={this.collapse}>
+            <h4><a href="#collapse">What is an access key?</a></h4>
+          </div>
+          <div className="panel-body" style={{display: "none"}}>
+            <p>It is the username equivalent. If you play with a bunch of
+            people, try to create as many access keys as you have players.</p>
+          </div>
+
+          <div className="panel-heading" onClick={this.collapse}>
+            <h4><a href="#collapse">Are there different access rights?</a></h4>
+          </div>
+          <div className="panel-body" style={{display: "none"}}>
+            <p>Right, the admin and no admin setup. It is planned to add
+            a role each access key, but right now everyone is the
+            superadmin. Make backups often if you do not trust your players!</p>
+          </div>
+        </div></div>
       </div>
     </div>;
   }
